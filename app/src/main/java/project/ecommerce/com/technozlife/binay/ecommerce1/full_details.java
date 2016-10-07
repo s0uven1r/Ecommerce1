34521e421256b4f230e393de422003ba156dc984
binay1234567890 <pikaju789@gmail.com>
2016-10-07 18:25:25 +0530

    full deteils activity for each item added.

---
+++ app/src/main/java/project/ecommerce/com/technozlife/binay/ecommerce1/full_details.java
@@ -0,0 +1,13 @@
+package project.ecommerce.com.technozlife.binay.ecommerce1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class full_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_details);
+    }
+}
